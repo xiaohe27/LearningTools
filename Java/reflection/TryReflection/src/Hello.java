c95ef790c8044628e053497ed689617ba4a6aae4
He Xiao <hexiao2@illinois.edu>
2014-11-24 10:01:16 -0600

    add folder Java

---
+++ Java/reflection/TryReflection/src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by xiaohe on 14-11-24.
+ */
+public class Hello {
+//    private String name;
+
+    public static void speak(String words){
+        System.out.println("Hi, there: "+words);
+    }
+}
---
+++ Java/reflection/TryReflection/src/ReflectionDemo.java
@@ -0,0 +1,8 @@
+/**
+ * Created by xiaohe on 14-11-24.
+ */
+public class ReflectionDemo {
+    public static void main(String[] args){
+        
+    }
+}
